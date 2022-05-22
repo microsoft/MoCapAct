54e128afa1b9abb1473c9ec003b14dc4a5eb2198
Felipe Vieira Frujeri <felipe.frujeri@microsoft.com>
2022-05-22 10:49:25 -0700

    Add Experiment config for ATAC in AML.

--- aml_experiments/environment_manager.py
+++ aml_experiments/environment_manager.py
@@ -3,27 +3,30 @@ from azureml.core.conda_dependencies import CondaDependencies
 
 from azureml.core import Environment
 
-def create_env(ws, env_name, requirements_file_path):
-    # env = Environment.get(workspace=ws, name="AzureML-pytorch-1.9-ubuntu18.04-py37-cuda11-gpu").clone(env_name)
-    env = Environment(name=env_name)
-    env.docker.base_image = 'mcr.microsoft.com/azureml/openmpi4.1.0-cuda11.1-cudnn8-ubuntu18.04:20220412.v1'
+def create_env(ws, env_name, requirements_file_path, version=None):
+    if version is not None:
+        env = Environment.get(workspace=ws, name=env_name, version=version)
+    else:
+        # env = Environment.get(workspace=ws, name="AzureML-pytorch-1.9-ubuntu18.04-py37-cuda11-gpu").clone(env_name)
+        env = Environment(name=env_name)
+        env.docker.base_image = 'mcr.microsoft.com/azureml/openmpi4.1.0-cuda11.1-cudnn8-ubuntu18.04:20220412.v1'
 
-    conda_dependencies = CondaDependencies()
-    conda_dependencies.set_python_version('3.7')
-    conda_dependencies.add_channel('menpo')
-    conda_dependencies.add_channel('conda-forge')
+        conda_dependencies = CondaDependencies()
+        conda_dependencies.set_python_version('3.7')
+        conda_dependencies.add_channel('menpo')
+        conda_dependencies.add_channel('conda-forge')
 
-    conda_dependencies.add_conda_package('pip==21.2.2')
-    conda_dependencies.add_conda_package('glew')
-    conda_dependencies.add_conda_package('mesalib')
-    conda_dependencies.add_conda_package('glfw3')
-    conda_dependencies.add_conda_package('imageio-ffmpeg')
+        conda_dependencies.add_conda_package('pip==21.2.2')
+        conda_dependencies.add_conda_package('glew')
+        conda_dependencies.add_conda_package('mesalib')
+        conda_dependencies.add_conda_package('glfw3')
+        conda_dependencies.add_conda_package('imageio-ffmpeg')
 
-    for p in Environment.from_pip_requirements(name="myenv", file_path=requirements_file_path).python.conda_dependencies.pip_packages:
-        conda_dependencies.add_pip_package(p)
+        for p in Environment.from_pip_requirements(name="myenv", file_path=requirements_file_path).python.conda_dependencies.pip_packages:
+            conda_dependencies.add_pip_package(p)
 
-    env.python.conda_dependencies = conda_dependencies
-    env.environment_variables['MUJOCO_GL'] = 'osmesa'
+        env.python.conda_dependencies = conda_dependencies
+        env.environment_variables['MUJOCO_GL'] = 'osmesa'
     env.register(ws)
     return env
 
--- aml_experiments/launch_offline_rl.py
+++ aml_experiments/launch_offline_rl.py
@@ -26,24 +26,39 @@ output_ref = OutputFileDatasetConfig(destination=(ws.get_default_datastore(), 'l
 #               'cp ./aml_experiments/tensorboard_patcher.py `python -m site --user-site`/usercustomize.py && ' +
 #               'pip install -e . && ')
 
-setup_cmds = ('pip install -e . && ')
+setup_cmds = ('pip install -e git+https://github.com/microsoft-fevieira/atac.git#egg=atac && pip install -e . && ')
+
+# script_run_config = ScriptRunConfig(
+#     source_directory=os.path.join(root_dir),
+#     command=[
+#         setup_cmds + 'python', './humanoid_control/offline_rl/train_bcq.py',
+#         "--dataset_local_path", input_ref,
+#         # "--output_root", 'output_ref',
+#         "--output_root", './logs',
+#         "--train_dataset_files", "CMU_016_22-0-82.hdf5",
+#         "--val_dataset_files", "CMU_016_22-0-82.hdf5",
+#         # "--eval_freq", "1",
+#         "--max_timesteps", "1e9",
+#         "--phi", "0",
+#         "--record_video", "True",
+#         "--normalize_obs", "True",
+#         "--normalize_act", "True",
+#         "--normalize_rew", "True",
+#     ],
+#     compute_target=compute_manager.create_compute_target(ws, 'gpu-NC24'),
+#     environment=environment_manager.create_env(ws, "hum-control-env", os.path.join(root_dir, 'requirements.txt'), version=15)
+# )
 
 script_run_config = ScriptRunConfig(
     source_directory=os.path.join(root_dir),
     command=[
-        setup_cmds + 'python', './humanoid_control/offline_rl/train_bcq.py',
+        setup_cmds + 'python', './humanoid_control/offline_rl/train_atac.py',
         "--dataset_local_path", input_ref,
         # "--output_root", 'output_ref',
-        "--output_root", './logs',
+        "--log_root", './logs',
         "--train_dataset_files", "CMU_016_22-0-82.hdf5",
         "--val_dataset_files", "CMU_016_22-0-82.hdf5",
-        # "--eval_freq", "1",
-        "--max_timesteps", "1e9",
-        "--phi", "0",
-        "--record_video", "True",
-        "--normalize_obs", "True",
-        "--normalize_act", "True",
-        "--normalize_rew", "True",
+        "--env_name", "MocapTrackingGymEnv",
     ],
     compute_target=compute_manager.create_compute_target(ws, 'gpu-NC24'),
     environment=environment_manager.create_env(ws, "hum-control-env", os.path.join(root_dir, 'requirements.txt'))
