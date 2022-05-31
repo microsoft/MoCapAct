d0d6274e165d3c6e5c95d8c322b102a6392f50ba
Nolan Wagener <nolan.wagener@gmail.com>
2022-05-31 17:46:25 -0400

    Add flag for mocap data directory

--- humanoid_control/clip_expert/evaluate.py
+++ humanoid_control/clip_expert/evaluate.py
@@ -1,3 +1,4 @@
+import os
 import os.path as osp
 import numpy as np
 import torch
@@ -22,6 +23,7 @@ FLAGS = flags.FLAGS
 flags.DEFINE_string("policy_root", None, "Path to the policy")
 flags.DEFINE_integer("min_steps", 10, "Minimum number of steps (used for RSI)")
 flags.DEFINE_float("act_noise", 0.1, "Action noise in humanoid")
+flags.DEFINE_string("mocap_dir", ".", "Directory where CMU mocap data is stored")
 
 # Visualization hyperparameters
 flags.DEFINE_bool("visualize", True, "Whether to visualize via GUI")
@@ -42,6 +44,9 @@ flags.mark_flag_as_required("policy_root")
 logging.set_verbosity(logging.WARNING)
 
 def main(_):
+    # Mocap directory
+    os.environ['CMU_MOCAP_DIR'] = FLAGS.mocap_dir
+
     # Make environment
     with open(osp.join(FLAGS.policy_root, osp.pardir, osp.pardir, 'clip_info.json')) as f:
         clip_info = json.load(f)
