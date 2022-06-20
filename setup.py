8f325c8e17569617f4d4a5046bdd7c2b5c48c5f0
Nolan Wagener <nolan.wagener@gatech.edu>
2022-06-20 15:59:21 -0400

    Properly set up setup script

--- setup.py
+++ setup.py
@@ -4,5 +4,17 @@ setup(
     name='mocapact',
     version='0.1',
     author='Nolan Wagener',
-    author_email='nolan.wagener@gatech.edu'
+    author_email='nolan.wagener@gatech.edu',
+    packages=['mocapact'],
+    install_requires=[
+        'cloudpickle<=2.1.0',
+        'dm_control<=1.0.2',
+        'h5py',
+        'imageio',
+        'imageio-ffmpeg',
+        'ml_collections',
+        'mujoco<=2.1.5',
+        'pytorch-lightning<=1.6.4',
+        'stable-baselines3'
+    ]
 )
