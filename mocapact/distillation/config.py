694350471712b7d1aeb761e59ce2f62f63135241
Nolan Wagener <nolan.wagener@gmail.com>
2022-06-20 22:32:49 -0400

    Remove unused code

--- mocapact/distillation/config.py
+++ mocapact/distillation/config.py
@@ -77,63 +77,8 @@ def get_config(config_string):
                 attn_pdrop=0.1,
                 weight_decay=0.1,
                 observables=observables.HIGH_LEVEL_OBSERVABLES_SANS_REFERENCE
-                ))
-            }),
-        #'hierarchical_gpt': ConfigDict({
-        #    'constructor': prepend_model('HierarchicalGPTPolicy'),
-        #    'config': ConfigDict(dict(
-        #        block_size=16,
-        #        n_layer=4,
-        #        n_head=4,
-        #        n_embd=768,
-        #        embd_pdrop=0.1,
-        #        resid_pdrop=0.1,
-        #        attn_pdrop=0.1,
-        #        observables=observables.HIERARCHICAL_OBSERVABLES
-        #        ))
-        #    }),
-        #'mixed_hierarchical': ConfigDict({
-        #    'constructor': prepend_model('MixedHierarchicalPolicy'),
-        #    'config': ConfigDict(dict(
-        #        block_size=16,
-        #        n_layer=4,
-        #        n_head=4,
-        #        n_embd=768,
-        #        embd_pdrop=0.1,
-        #        resid_pdrop=0.1,
-        #        attn_pdrop=0.1,
-        #        embed_size=20,
-        #        layer_norm=True,
-        #        decoder_n_layers=3,
-        #        decoder_layer_size=1024,
-        #        kl_weight=0.002,
-        #        activation_fn='torch.nn.Tanh',
-        #        squash_output=True,
-        #        action_decoder_load_path="",
-        #        observables=observables.MIXED_HIERARCHICAL_OBSERVABLES
-        #        ))
-        #    }),
-        #'hierarchical': ConfigDict({
-        #    'constructor': prepend_model('HierarchicalPolicy'),
-        #    'config': ConfigDict(dict(
-        #        embed_size=20,
-        #        ref_encoder_n_layers=2,
-        #        ref_encoder_layer_size=1024,
-        #        stand_encoder_n_layers=2,
-        #        stand_encoder_layer_size=1024,
-        #        decoder_n_layers=3,
-        #        decoder_layer_size=1024,
-        #        stand_vf_n_layers=3,
-        #        stand_vf_layer_size=1024,
-        #        layer_norm=True,
-        #        embedding_kl_weight=0.1,
-        #        embedding_correlation=0.95,
-        #        seq_steps=30,
-        #        activation_fn='torch.nn.Tanh',
-        #        squash_output=False,
-        #        observables=observables.HYBRID_HIERARCHICAL_OBSERVABLES
-        #    ))
-        #})
+            ))
+        }),
     }
 
     return configs[config_string]
