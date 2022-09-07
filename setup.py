271448aba3149acf4a1760bf59f44436dddab75d
Nolan Wagener <nolan.wagener@gmail.com>
2022-09-07 13:41:48 -0400

    Update dependencies

--- setup.py
+++ setup.py
@@ -10,12 +10,12 @@ setup(
     install_requires=[
         'azure.storage.blob==12.9.0',
         'cloudpickle>=2.1.0',
-        'dm_control==1.0.2',
+        'dm_control>=1.0.7',
         'h5py',
         'imageio',
         'imageio-ffmpeg',
         'ml_collections',
-        'mujoco==2.1.5',
+        'mujoco',
         'pytorch-lightning<1.7',
         'stable-baselines3'
     ]
