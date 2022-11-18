8b3e313289969e98915c8ae5dab0ca21c8c81d18
Nolan Wagener <nolan.wagener@gmail.com>
2022-11-18 17:54:53 -0500

    Keep gym fixed to 0.21 to maintain compatibility with our environment definitions

--- setup.py
+++ setup.py
@@ -11,6 +11,7 @@ setup(
         'azure.storage.blob==12.9.0',
         'cloudpickle>=2.1.0',
         'dm_control>=1.0.7',
+        'gym==0.21',
         'h5py',
         'imageio',
         'imageio-ffmpeg',
