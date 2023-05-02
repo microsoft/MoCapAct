83dd28d1df7c7371e7bcde69652a737ec28db67e
Nolan Wagener <nolan.wagener@gmail.com>
2023-05-02 09:22:01 -0400

    Minor error message change

--- mocapact/distillation/dataset.py
+++ mocapact/distillation/dataset.py
@@ -317,7 +317,7 @@ class ExpertDataset(Dataset):
         TODO
         """
         if idx >= len(self):
-            raise IndexError("dataset index out of range")
+            raise IndexError("Dataset index out of range")
 
         dset_idx = bisect.bisect_right(self._dset_indices, idx)-1
 
